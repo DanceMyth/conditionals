77dee17ccc2a797a4734aa26277502cf98a7b15a
Dan Smith <Dancemyth@icloud.com>
2015-12-02 15:20:06 +0000

    first commit

---
+++ Conditionals.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+var isMyHouseOnFire:Bool = true
+
+if isMyHouseOnFire {
+    print("water please")
+    }
+else{
+print("fire please")
+}
+
+
+
+
---
+++ conditionals 2.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+func funkA()
+{
+    print("this is my first function" )
+
+}
+
+funkA()
+
+var age = 65
+var money = 90000.00
+var richUncleDied = false
+
+func canIRetire()
+{
+    if age >= 60 && money >= 100000.00 || richUncleDied
+    {
+        "you can retire"
+    }
+    else
+    {
+        "you cannot retire"
+    }
+}
+
+canIRetire()
